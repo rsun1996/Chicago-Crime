cd7179896873acb8c5483fabf4559b5b2b26b817
Luis Treviño <45924002+leotrevino@users.noreply.github.com>
2021-10-10 19:27:55 -0500

    Add files via upload

---
+++ Code/Project_Code_004.R
@@ -0,0 +1,195 @@
+library(rvest) 
+library(plyr)
+library(dplyr)
+library(textreadr)
+
+### Reading in Crimes data set ################################################
+Crimes <- read.csv("C:\\Users\\Sudo\\Desktop\\Crimes_2001_to_Present.csv")
+###############################################################################
+
+names(Crimes)
+
+#Dropping unnecessary columns
+Crimes <- Crimes[-c(1:2,4:5,7:8,11:13,15:22)] 
+
+# Splitting Date column into Date and Time and AM/PM
+Crimes$Date_2 <- sapply(strsplit(as.character(Crimes$Date), " "), "[", 1)
+Crimes$Time <- sapply(strsplit(as.character(Crimes$Date), " "), "[", 2)
+Crimes$AM.PM <- sapply(strsplit(as.character(Crimes$Date), " "), "[", 3)
+
+#Deleting old date column
+Crimes <- Crimes[-c(1)]
+
+#Renaming new date column
+names(Crimes)[5] <- 'Date'
+
+#Reordering columns
+Crimes <- Crimes[,c(5:7,1:4)]
+
+
+
+########################### Web Scrapping of White Sox Games ###########################
+
+#Get years
+years <- c("2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010",
+           "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021")
+
+#Fill with the following loop. Data frame for all games.
+all_games <- data.frame(Date=character(), WL=character(), DN=character(),
+                        Attendance=integer(), Year=integer())
+
+for(i in 1:length(years)){
+  
+  #Game schedule url
+  scheduleurl <- paste0("https://www.baseball-reference.com/teams/CHW/",years[i],"-schedule-scores.shtml")
+  
+  #Read schedule url
+  schedulehtml <- read_html(scheduleurl)
+  
+  #Schedule table from website
+  schedule_results <- html_nodes(schedulehtml,"#team_schedule")
+  
+  #Create a table
+  schedule_table <- html_table(schedule_results)
+  
+  #Schedule results into a data frame
+  schedule_data <- data.frame(schedule_table)
+  schedule_data <- schedule_data %>% dplyr::rename(Home_Away = 5)
+  
+  #Add year to each game
+  schedule_data$Year <- years[i]
+  
+  #Keep Date, Home/Away, Opponent, Win/Loss, D/N, Attendance, and Year
+  schedule_data <- schedule_data[, c(2,5,6,7,18,19,23)]
+  
+  #Make 'Attendance' and 'Year' numeric
+  schedule_data$Attendance <- as.numeric(gsub(",","",schedule_data$Attendance))
+  schedule_data$Year <- as.numeric(schedule_data$Year)
+  
+  #Remove column headers in data
+  schedule_data <- schedule_data[which(schedule_data$Opp != "Opp"),]
+  
+  #Remove away games
+  schedule_data <- schedule_data[which(schedule_data$Home_Away != "@"),]
+  
+  #Remove Home/Away column
+  schedule_data <- schedule_data[, c(1,3,4,5,6,7)]
+  
+  #Putting all the data into all_games
+  all_games <- rbind(all_games, schedule_data)
+}
+
+#save(all_games, file = "chw_game_results.RData")
+#load(file = "chw_game_results.RData")
+
+### All_games (White Sox games) data set ###########################
+Games <- all_games
+###############################################################################
+
+
+###NOTES:######################################################################
+#Chicago is organized into 77 community areas.
+
+table(Crimes$Community.Area) #There are a total of 77 community areas in the Crimes data set.
+
+#Guaranteed Rate Field (White Sox Stadium) is located in the Bridgeport neighborhood (COMMUNITY AREA 60).
+
+#There are 43331 total crime reports in community area 60 specifically 
+###############################################################################
+
+#Matching Crimes data set and Games data set (by Date)
+
+#Date-time conversion of Games date column: %A, %b %d TO %b %d (leaving out 'day of the week,')
+Games$Date <- format(as.POSIXct(Games$Date,format='%A, %b %d'), format='%b %d')
+
+#Concatenating columns involving dates (grouping month, day, and year into one column)
+Games$Date_grouped <- paste(Games$Date, Games$Year)
+
+# Date-time conversion of Games date column: %b %d %Y TO %m/%d/%Y
+Games$Date_grouped <- format(as.POSIXct(Games$Date_grouped,format='%b %d %Y'), format='%m/%d/%Y')
+
+#Deleting old date column
+names(Games)
+Games <- Games[-(1)]
+
+#Renaming new date column
+names(Games)[6] <- 'Date'
+
+#Sorting columns so date is first
+Games <- Games[, c(6, 1:5)]
+
+#Now dates from Crimes and Games match#
+
+
+
+#Inner Join (By matching dates - 1.7million)
+# test <- merge(Crimes, Games, by = 'Date')
+
+#Left Join with dplyr 
+Crimes_Games <- left_join(Crimes, Games, by = c("Date"))
+
+
+#Adding day of week column
+Crimes_Games$Day <- format(as.POSIXct(Crimes_Games$Date,format='%m/%d/%Y'), format='%A')
+
+
+#Addition of new column, game day, which is 1 for TRUE (Date was a game day) and 0 for FALSE (Date was not a game day).
+Crimes_Games$Gameday[Crimes_Games$Attendance >= 0] <- 1
+Crimes_Games$Gameday[is.na(Crimes_Games$Attendance)] <- 0
+
+
+#Keeping only observations with community area 60 and 34
+Crimes_Games <- Crimes_Games[which(Crimes_Games$Community.Area == 34 | Crimes_Games$Community.Area == 60),]
+
+#Aggregate
+table1 <- data.frame(table(Crimes_Games$Date))
+names(table1)[1] <- 'Date'
+
+
+final <- merge(Crimes_Games, table1, by = 'Date')
+
+
+
+# Fitted regression tests
+
+p <- lm(formula = Freq ~ Primary.Type + Day + Gameday, data = final)
+summary(p)
+
+
+q <- lm(formula = Freq ~ Gameday + Day, data=final)
+lm(q)
+
+
+
+##### Other #####
+
+merge1$Time <- format(as.POSIXct(as.character(merge1$Time)), "%H:%M:%S")
+dat[dat$hms >= "09:00:00" & dat$hms <= "10:00:00",]
+
+
+
+#D/N
+
+for (i in 1:nrow(Crimes_Games)) {
+  
+  if (Crimes_Games$AM.PM == 'AM' & Crimes_Games$Time ) {
+    
+    Crimes_Games$Day.Night == 'D' }
+  
+}
+
+
+names(merge1)
+
+lm(formula = Freq ~ Date + Primary.Type + Arrest + Domestic + Community.Area + Day + Gameday, data=merge1)
+
+summary
+
+
+Date <- seq.Date(from=as.Date("2001-01-01"), to=as.Date("2021-09-04"), by="day")
+
+Date
+
+
+
+
